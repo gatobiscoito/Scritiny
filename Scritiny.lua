37f91486b9858585ca26752c02b2d11d8bc69b48
gatobiscoito <157207169+gatobiscoito@users.noreply.github.com>
2024-01-20 22:05:51 +0900

    Update Scritiny.lua

--- Scritiny.lua
+++ Scritiny.lua
@@ -1,9 +1,9 @@
 <?
 -- Variables definition
-videoFps = 60
-objTime = obj.totalframe
+videoFps    = 60
+objTime     = obj.totalframe
 totalSecond = (objTime / videoFps) * 1000
-runMinute = (math.floor(totalSecond)) / 60000
+runMinute   = (math.floor(totalSecond)) / 60000
 clearMinute = math.floor(runMinute)
 clearSecond = (runMinute - clearMinute) * 60
 
